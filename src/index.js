9b35ac48aae134fdb8a40931ddc401859b5446f8
Dory <78720364+dooory@users.noreply.github.com>
2026-04-16 13:21:50 +1200

    feat: add custom validation for email field

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./style.css";
 
-console.log("My Webpack template!");
+const emailInput = document.getElementById("email");
+
+const validateEmail = () => {
+    emailInput.reportValidity();
+
+    const validity = emailInput.validity;
+
+    if (validity.valueMissing) {
+        emailInput.setCustomValidity("Please enter an email.");
+    } else if (validity.typeMismatch) {
+        emailInput.setCustomValidity("Please enter a valid email.");
+    } else {
+        emailInput.setCustomValidity("");
+    }
+};
+
+emailInput.addEventListener("blur", validateEmail);
